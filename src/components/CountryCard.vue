d4dd15eda63be9dc59ead2e58b08dfb7e178c56e
vmelnikova <vmelnikova@phoenixit.ru>
2026-06-15 13:04:06 +0300

    правка стилей

--- src/components/CountryCard.vue
+++ src/components/CountryCard.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="card">
-      <span>{{ country?.continent.code }}</span>
-
       <div>
-        <div class="d-flex flex-column">
-          <span>{{ country?.name }}</span>
+        <div class="d-flex flex-column ga-1">
+          <span class="title">{{ country?.name }}</span>
           <span class="caption">{{ country?.capital || '—' }}</span>
         </div>
         <v-chip
@@ -12,6 +10,7 @@
           :color="CONTINENT_COLORS[country?.continent.name].bg"
           :style="{ color: CONTINENT_COLORS[country?.continent.name].text }"
           variant="flat"
+          class="mt-1"
         >
           {{ country?.continent.code }}
         </v-chip>
@@ -28,7 +27,7 @@ defineProps<{ country: Country }>();
 
 <style scoped>
 .card {
-  background: var(--background);
+  background: var(--background-secondary);
   border-radius: 15px;
   border: 1px solid var(--border);
   color: #f1f5f9;
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,19 +1,29 @@
 <template>
   <div class="page">
     <h1 class="mb-1">Countries of the World</h1>
-    <span>250 countries — explore capitals, currencies, languages and more</span>
+    <span class="subtitle">250 countries — explore capitals, currencies, languages and more</span>
 
     <v-text-field
       prepend-inner-icon="mdi mdi-magnify"
-      placeholder="Search by name, code or capital…"
+      placeholder="Search by name, code or capital"
       rounded
+      clearable
+      variant="outlined"
       class="search-field"
     />
 
     <div class="d-flex ga-2">
-      <v-chip v-for="continent of Continents" variant="outlined" class="chip">
-        {{ continent }}
-      </v-chip>
+      <v-chip-group v-model="continentFilter" selected-class="chip_selected">
+        <v-chip
+          v-for="continent of Continents"
+          :key="continent"
+          :value="continent"
+          variant="outlined"
+          class="chip"
+        >
+          {{ continent }}
+        </v-chip>
+      </v-chip-group>
     </div>
 
     <v-container class="mt-3">
@@ -39,10 +49,13 @@ import { GET_COUNTRIES } from '@/apollo/queries';
 import type { GetCountriesQuery, GetCountriesQueryVariables } from '@/apollo/types/graphql';
 import CountryCard from '@/components/CountryCard.vue';
 import { Continents } from '@/constants';
+import { ref } from 'vue';
 
 const { result, loading, error, refetch } = useQuery<GetCountriesQuery, GetCountriesQueryVariables>(
   GET_COUNTRIES
 );
+
+const continentFilter = ref(Continents.All);
 </script>
 
 <style scoped lang="scss">
@@ -50,17 +63,34 @@ const { result, loading, error, refetch } = useQuery<GetCountriesQuery, GetCount
   flex: 1;
 }
 
+.subtitle {
+  color: var(--text-secondary);
+}
+
 .chip {
   background-color: var(--background-secondary);
-  color: var(--text);
+  color: var(--text-secondary);
   border-color: var(--border);
+
+  &_selected {
+    background-color: var(--background-primary);
+    color: var(--text-primary);
+    border-color: var(--text-primary);
+  }
 }
 
 .search-field {
-  margin-top: 30px;
+  margin-top: 20px;
 
-  .v-input__control {
-    background-color: var(--background-secondary) !important;
+  :deep(.v-input__control) {
+    .v-field__outline {
+      //background-color: var(--background-secondary) !important;
+    }
+
+    .v-field__input {
+      padding: 6px !important;
+      min-height: 42px !important;
+    }
   }
 }
 </style>
